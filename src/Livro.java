c7908ff74446b1d2a0ba08d32664ea5e55c083e7
raylanalvesST <raylanalves.br@gmail.com>
2023-11-10 08:01:17 -0300

    Alterações: Criação da Classe Livro

---
+++ src/Livro.java
@@ -0,0 +1,53 @@
+//ATRIBUTOS----------------------------------------------------------------------------
+private String titulo;
+private String autor;
+private String localizacao;
+
+//MÉTODOS------------------------------------------------------------------------------
+public void consultarLivro(){
+    System.out.println("Livro: " + getTitulo() + ", autor: " + getAutor() + ", localizado: " + getLocalizacao()); }
+
+public void emprestarLivro(String localizacao){ this.localizacao = localizacao; }
+
+public void devolverLivro(){ this.localizacao = "Biblioteca"; }
+
+//MÉTODO CONSTRUTOR--------------------------------------------------------------------
+public Livro(String titulo, String autor) {
+    this.titulo = titulo;
+    this.autor = autor;
+    this.localizacao = "Biblioteca";
+}
+
+//GETTERS AND SETTERS------------------------------------------------------------------
+public String getTitulo() {
+    return titulo;
+}
+
+public void setTitulo(String titulo) {
+    this.titulo = titulo;
+}
+
+public String getAutor() {
+    return autor;
+}
+
+public void setAutor(String autor) {
+    this.autor = autor;
+}
+
+public String getLocalizacao() {
+    return localizacao;
+}
+
+public void setLocalizacao(String localizacao) {
+    this.localizacao = localizacao;
+}
+
+//ToSTRING---------------------------------------------------------------------------
+@Override
+public String toString() {
+    return "Livro{" + "titulo='" + titulo + '\'' +
+            ", autor='" + autor + '\'' +
+            ", localizacao='" + localizacao + '\'' +
+            '}';
+}
