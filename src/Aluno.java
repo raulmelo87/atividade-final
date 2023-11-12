02869b5d013caf6d8553cc21076a724ad95ba487
alanna-mercia <mercia0809@hotmail.com>
2023-11-12 16:12:39 -0300

    Adicionei Classe Aluno

---
+++ src/Aluno.java
@@ -0,0 +1,48 @@
+public class Aluno extends Pessoa{
+    //ATRIBUTOS----------------------------------------------------------------------------
+    private float nota;
+    private String curso;
+
+    //MÉTODOS------------------------------------------------------------------------------
+    public String estaAprovado (float nota){
+
+        if (nota >6){
+            return "aprovado";
+        }else {
+            return "reprovado";
+        }
+
+    }
+
+    //MÉTODO CONSTRUTOR---------------------------------------------------------------------
+    public Aluno(String nome, int idade, String sexo, String curso, float nota) {
+        super(nome, idade, sexo);
+        this.nota = nota;
+        this.curso = curso;
+    }
+
+    //GETTERS E SETTERS---------------------------------------------------------------------
+    public String getCurso() {
+        return curso;
+    }
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public float getNota() {
+        return nota;
+    }
+    public void setNota(float nota) {
+        this.nota = nota;
+    }
+
+    //TOSTRING--------------------------------------------------------------------------------
+    @Override
+    public String toString() {
+        return"Aluno{" +
+                "Curso= " + curso +
+                ", Nota= " + nota +
+                "}" + super.toString();
+    }
+
+}
