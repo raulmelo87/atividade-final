06da58293ee7d663b680a42f7cf5a4846190fff1
raulmelo87 <fraulbmelo@gmail.com>
2023-11-18 15:31:22 -0300

    Revisão e ajustes finais

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 Trabalho da Faculdade UNIFOR, curso Análise e Desenvolvimento de Sistemas.
+Professor: Ronaldo Pinheiro
+Membros da equipe:
+Francisco Raul Bezerra Melo - 2319731
+Alanna Mércia de França Vidal - 2313687
+Laiza Silva Fernandes - 2317162
+Francisco Edson da Silva Gama - 2317663
+Raylan Pereira Alves - 2319016
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         System.out.println("---------------------------------------------------------------------------------");
-        Livro li1 = new Livro("Sapines", "Yuval Harari");
+        Livro li1 = new Livro("Sapiens", "Yuval Harari");
         li1.consultarLivro();
         System.out.println("---------------------------------------------------------------------------------");
 
